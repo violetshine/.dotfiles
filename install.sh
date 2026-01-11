dc32713081aab5d7ae412420c0b34d186e958dc6
Violet <me@violetshine.net>
2026-01-11 17:34:19 +0200

    direnv: add direnv.toml

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ USERFOLDER=~
 # zsh
 ln -f $USERFOLDER/.dotfiles/zsh/zshrc $USERFOLDER/.zshrc
 
+# direnv
+ln -F -s $USERFOLDER/.dotfiles/direnv $USERFOLDER/.config/direnv
+
 # nvim
 ln -F -s $USERFOLDER/.dotfiles/nvim $USERFOLDER/.config/nvim
